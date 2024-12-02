8060eb1c2cd4ce4e18086af48297bd0c5b8d298c
Mapali321 <mapali321@gmail.com>
2024-12-02 18:42:18 -0500

    Update main.blocks, main.ts, _history

--- main.ts
+++ main.ts
@@ -5,7 +5,7 @@ input.onGesture(Gesture.Shake, function () {
 })
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     basic.showIcon(IconNames.Happy)
-    music._playDefaultBackground(music.builtInPlayableMelody(Melodies.JumpUp), music.PlaybackMode.UntilDone)
+    music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Baddy), music.PlaybackMode.UntilDone)
     basic.showIcon(IconNames.Asleep)
 })
 basic.showIcon(IconNames.Asleep)
